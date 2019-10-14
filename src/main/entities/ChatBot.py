070c0e1824ee9855aac0630b19c58a981dbd18d1
I508685 <henrique.padovani@sap.com>
2019-10-13 23:45:25 -0300

    comment chatbot lines

--- src/main/entities/ChatBot.py
+++ src/main/entities/ChatBot.py
@@ -1,7 +1,7 @@
 
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+# from chatterbot.trainers import ChatterBotCorpusTrainer
 
 
 
@@ -10,13 +10,13 @@ def getChatBot(chatbotName):
 
     chatbot = ChatBot(chatbotName)
     # Create a new trainer for the chatbot
-    trainer = ChatterBotCorpusTrainer(chatbot)
-    # Train the chatbot based on the english corpus
-    trainer.train("chatterbot.corpus.english")
-    # Train based on english greetings corpus
-    trainer.train("chatterbot.corpus.english.greetings")
-    # Train based on the english conversations corpus
-    trainer.train("chatterbot.corpus.english.conversations")
+    # trainer = ChatterBotCorpusTrainer(chatbot)
+    # # Train the chatbot based on the english corpus
+    # trainer.train("chatterbot.corpus.english")
+    # # Train based on english greetings corpus
+    # trainer.train("chatterbot.corpus.english.greetings")
+    # # Train based on the english conversations corpus
+    # trainer.train("chatterbot.corpus.english.conversations")
     # Get a response to an input statement
     return chatbot
 
