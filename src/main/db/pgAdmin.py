1c734966a4db40b73f099ca12d0714630eebc55c
I508685 <henrique.padovani@sap.com>
2019-10-13 23:41:52 -0300

    add db postgres from heroku

--- src/main/db/pgAdmin.py
+++ src/main/db/pgAdmin.py
@@ -6,6 +6,12 @@ class MenagerDB:
     cursor = None
 
     def __init__(self, dataBaseName):
-        self.db = psycopg2.connect("dbname=" + str(dataBaseName) + " user=postgres password=henrique901")
+        # self.db = psycopg2.connect("dbname=" + str(dataBaseName) + " user=postgres password=henrique901")
+        host = 'ec2-54-83-55-125.compute-1.amazonaws.com'
+        db = 'd7g9aq8m04v1dl'
+        user = 'jumuxuqmyofhdw'
+        port = '5432'
+        pw = '77daa1d66905f7d02832b39dfb4bc9896e4fb9cd26bf314bcb4d869b91f78101'
+        self.db = psycopg2.connect(dbName = db, user=user, password=pw, host=host, port=port)
         self.cursor = self.db.cursor()
         return
