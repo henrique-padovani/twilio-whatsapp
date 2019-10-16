b9e18c86d2c948e33e5d6c18bc2c0d3a305ad02c
I508685 <henrique.padovani@sap.com>
2019-10-15 22:23:18 -0300

    fix

--- src/main/controllers/whatsappController.py
+++ src/main/controllers/whatsappController.py
@@ -4,8 +4,9 @@ from src.main.db.MessageDB import MessageDB
 
 class WhatsApp(Resource):
     def post(self):
-        message = request.data
-        print(message) 
+        message = request.form
+        print("request.form: " + str(request.form))
+        print("get_json: " + str(request.get_json(force=True) ))
         MessageDB.saveMessage(message)
         return jsonify("Rodou...")
 
