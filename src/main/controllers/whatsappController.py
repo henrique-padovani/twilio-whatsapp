afb9d3730c43ef94ef8b6001dfed98ef9016bddd
I508685 <henrique.padovani@sap.com>
2019-10-16 09:24:46 -0300

    Add improvements

--- src/main/controllers/whatsappController.py
+++ src/main/controllers/whatsappController.py
@@ -5,8 +5,6 @@ from src.main.entities.ChatBot import Bot
 
 from twilio.rest import Client
 
-ACCOUNT_SID = 'ACc5382bd8ed2e7f906347594a71a8cd75'
-AUTH_TOKEN = '909cfa79c4508fc7506e4c9c545f5a8a'
 mybot_whats = 'whatsapp:+14155238886'
 
 class WhatsApp(Resource):
@@ -16,7 +14,7 @@ class WhatsApp(Resource):
         body = message['Body']
         to = message['From']
         
-        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+        client = Client()
         msg = Bot.getBotResponse(body)
         client.messages.create(body=msg, from_=mybot_whats, to=to)
 
